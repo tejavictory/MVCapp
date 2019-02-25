f05d5171977bc0bdbd311b7ef8888aaa2de0a4f5
Mallikarjuna Bodepudi <bmallikarjuna66@gmail.com>
2019-02-25 15:52:59 -0600

    Added my models Degree, Credit, DegreeCredit

---
+++ WebApplication2/Models/Degree.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebApplication2.Models
+{
+    public class Degree
+    {
+        public int DegreeID { get; set; }
+        public string DegreeAbbr { get; set; }
+        public string DegreeName { get; set; }
+        public int NumberOfTerms { get; set; }
+
+
+
+    }
+}
---
+++ WebApplication2/Models/DegreeCredit.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace WebApplication2.Models
+{
+    public class Credit
+    {
+        public int CreditID { get; set; }
+        public string CreditAbbr { get; set; }
+        public string CreditName { get; set; }
+        public int IsSummer { get; set; }
+        public int IsSpring { get; set; }
+        public int IsFall { get; set; }
+
+
+
+
+       
+
+
+    }
+}
