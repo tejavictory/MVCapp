28552afa11619225268e2502426db6390756fcc7
mouni krishna <mounikrishnaa@gmail.com>
2019-02-25 15:49:50 -0600

    changed String to string

--- WebApplication2/Models/DegreePlan.cs
+++ WebApplication2/Models/DegreePlan.cs
@@ -6,8 +6,8 @@ namespace WebApplication2.Models
     {
         public int DegreePlanID { get; set; }
         public int StudentID { get; set; }
-        public String DegreePlanAbbr { get; set; }
-        public String DegreePlanName { get; set; }
+        public string DegreePlanAbbr { get; set; }
+        public string DegreePlanName { get; set; }
         public int DegreePlanSelected { get; set; }
 
 
--- WebApplication2/Models/Student.cs
+++ WebApplication2/Models/Student.cs
@@ -5,7 +5,11 @@ namespace WebApplication2.Models
     public class Student
     {
         public int StudentID { get; set; }
+        public string Family { get; set; }
+        public string Given { get; set; }
+        public int _919N { get; set; }
 
-        
+
+
     }
 }
