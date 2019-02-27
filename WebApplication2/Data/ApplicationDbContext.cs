0ee3e9535fbd875991fadf4f47541cd2e3fe2a90
Krishna Teja <s533708@nwmissouri.edu>
2019-02-27 15:17:03 -0600

    Added code to Model Builder

--- WebApplication2/Data/ApplicationDbContext.cs
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -12,5 +12,24 @@ namespace WebApplication2.Data
             : base(options)
         {
         }
+        public DbSet<Degree> Degrees {get;set;}
+        public DbSet<Credit> Credits {get;set;}
+        public DbSet<DegreeCredit> DegreeCredits {get;set;}
+        public DbSet<DegreePlan> DegreePlans {get;set;}
+        public DbSet<Student> Students {get;set;}
+        public DbSet<Slot> Slots {get;set;}
+        public DbSet<StudentTerm> StudentTerms {get;set;}
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Degree>().ToTable("Degree");
+            modelBuilder.Entity<Credit>().ToTable("Credit");
+            modelBuilder.Entity<DegreeCredit>().ToTable("DegreeCredit");
+            modelBuilder.Entity<DegreePlan>().ToTable("DegreePlan");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Slot>().ToTable("Slot");
+            modelBuilder.Entity<StudentTerm>().ToTable("StudentTerm");
+
+
+        }
     }
 }
