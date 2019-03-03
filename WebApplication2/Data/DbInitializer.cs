5ce514b7c55bfcd690bc61a52812cc919098040c
Mallikarjuna Bodepudi <bmallikarjuna66@gmail.com>
2019-03-03 16:14:33 -0600

    Seeded with Credit and DegreeCredit data

--- WebApplication2/Data/DbInitializer.cs
+++ WebApplication2/Data/DbInitializer.cs
@@ -36,9 +36,110 @@ namespace WebApplication2.Data
 
             //for Credit - Malli
 
+            if (context.Credits.Any())
+            {
+                Console.WriteLine("Credits already exist.");
+            }
+            else
+            {
+                var credits = new Credit[]
+                {
+                    new Credit {CreditID=460, CreditAbbr='DB', CreditName='Databases', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=356, CreditAbbr='NF', CreditName='Network Fundamentals', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=542, CreditAbbr='542', CreditName='OOPS with Java', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=563, CreditAbbr='563', CreditName='Web Apps', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=560, CreditAbbr='560', CreditName='Advanced Databases', IsSummer=1, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=664, CreditAbbr='664-UX', CreditName='User Experience', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=618, CreditAbbr='618-PM', CreditName='Project Management', IsSummer=1, IsSpring=0, IsFall=0},
+                    new Credit {CreditID=555, CreditAbbr='555-NS', CreditName='Network Security', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=691, CreditAbbr='691-GDP1', CreditName='GDP1', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=692, CreditAbbr='692-GDP2', CreditName='GDP2', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=6, CreditAbbr='Mobile', CreditName='643 or 644 Mobile', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=10, CreditAbbr='E1', CreditName='Elective 1', IsSummer=0, IsSpring=1, IsFall=1},
+                    new Credit {CreditID=20, CreditAbbr='E2', CreditName='Elective 2', IsSummer=0, IsSpring=1, IsFall=1},
+
+                };
+                Console.WriteLine($"Inserted {credits.Length} new credits.");
+
+                foreach (Credit d in credits)
+                {
+                    context.Credits.Add(d);
+                }
+                context.SaveChanges();
+            }
+
+
             //for Student - Mouni
 
             //for DegreeCredit - Malli
+
+            if (context.DegreeCredits.Any())
+            {
+                Console.WriteLine("DegreeCredits already exist.");
+            }
+            else
+            {
+                var degreeCredits = new DegreeCredit[]
+                {
+                    new DegreeCredit {DegreeCreditID=1, DegreeID=1, CreditID=460},
+                    new DegreeCredit {DegreeCreditID=2, DegreeID=1, CreditID=356},
+                    new DegreeCredit {DegreeCreditID=3, DegreeID=1, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=4, DegreeID=1, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=5, DegreeID=1, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=6, DegreeID=1, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=7, DegreeID=1, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=8, DegreeID=1, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=9, DegreeID=1, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=10, DegreeID=1, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=11, DegreeID=1, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=12, DegreeID=1, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=13, DegreeID=1, CreditID=20},
+                    new DegreeCredit {DegreeCreditID=14, DegreeID=2, CreditID=460},
+                    new DegreeCredit {DegreeCreditID=15, DegreeID=2, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=16, DegreeID=2, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=17, DegreeID=2, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=18, DegreeID=2, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=19, DegreeID=2, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=20, DegreeID=2, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=21, DegreeID=2, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=22, DegreeID=2, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=23, DegreeID=2, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=24, DegreeID=2, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=25, DegreeID=2, CreditID=20},
+                    new DegreeCredit {DegreeCreditID=26, DegreeID=3, CreditID=356},
+                    new DegreeCredit {DegreeCreditID=27, DegreeID=3, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=28, DegreeID=3, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=29, DegreeID=3, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=30, DegreeID=3, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=31, DegreeID=3, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=32, DegreeID=3, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=33, DegreeID=3, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=34, DegreeID=3, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=35, DegreeID=3, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=36, DegreeID=3, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=37, DegreeID=3, CreditID=20},
+                    new DegreeCredit {DegreeCreditID=38, DegreeID=4, CreditID=542},
+                    new DegreeCredit {DegreeCreditID=39, DegreeID=4, CreditID=563},
+                    new DegreeCredit {DegreeCreditID=40, DegreeID=4, CreditID=560},
+                    new DegreeCredit {DegreeCreditID=41, DegreeID=4, CreditID=664},
+                    new DegreeCredit {DegreeCreditID=42, DegreeID=4, CreditID=618},
+                    new DegreeCredit {DegreeCreditID=43, DegreeID=4, CreditID=555},
+                    new DegreeCredit {DegreeCreditID=44, DegreeID=4, CreditID=691},
+                    new DegreeCredit {DegreeCreditID=45, DegreeID=4, CreditID=692},
+                    new DegreeCredit {DegreeCreditID=46, DegreeID=4, CreditID=6},
+                    new DegreeCredit {DegreeCreditID=47, DegreeID=4, CreditID=10},
+                    new DegreeCredit {DegreeCreditID=48, DegreeID=4, CreditID=20},
+
+                };
+                Console.WriteLine($"Inserted {degreeCredits.Length} new degree credits.");
+
+                foreach (DegreeCredit d in degreeCredits)
+                {
+                    context.DegreeCredits.Add(d);
+                }
+                context.SaveChanges();
+            }
+
 
             //for DegreePlan - Mouni 
 
