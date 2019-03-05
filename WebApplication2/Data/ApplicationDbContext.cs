5f0fb0b54ef1ad5c7e4f12edd40dc016d7df9890
Krishna Teja <s533708@nwmissouri.edu>
2019-03-05 16:04:26 -0600

    Changed the order for the models.

--- WebApplication2/Data/ApplicationDbContext.cs
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -17,23 +17,22 @@ namespace WebApplication2.Data
         }
         public DbSet<Degree> Degrees {get;set;}
         public DbSet<Credit> Credits {get;set;}
+        public DbSet<Student> Students { get; set; }
         public DbSet<DegreeCredit> DegreeCredits {get;set;}
         public DbSet<DegreePlan> DegreePlans {get;set;}
-        public DbSet<Student> Students {get;set;}
         public DbSet<Slot> Slots {get;set;}
         public DbSet<StudentTerm> StudentTerms {get;set;}
         
+
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Degree>().ToTable("Degree");
             modelBuilder.Entity<Credit>().ToTable("Credit");
+            modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<DegreeCredit>().ToTable("DegreeCredit");
             modelBuilder.Entity<DegreePlan>().ToTable("DegreePlan");
-            modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<Slot>().ToTable("Slot");
             modelBuilder.Entity<StudentTerm>().ToTable("StudentTerm");
-
-
         }
     }
 }
