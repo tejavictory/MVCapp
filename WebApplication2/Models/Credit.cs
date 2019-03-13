1996ed874e16e34184550a2e7ce76796fb9bcf41
mouni krishna <mounikrishnaa@gmail.com>
2019-03-13 15:49:12 -0500

    Added annotations to Degree.cs and Credit.cs

--- WebApplication2/Models/Credit.cs
+++ WebApplication2/Models/Credit.cs
@@ -9,10 +9,15 @@ namespace WebApplication2.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CreditId { get; set; }
+        [StringLength(10)]
         public string CreditAbbr { get; set; }
+        [StringLength(20)]
         public string CreditName { get; set; }
+        [Range(0,1)]
         public int IsSummer { get; set; }
+        [Range(0, 1)]
         public int IsSpring { get; set; }
+        [Range(0, 1)]
         public int IsFall { get; set; }
 
 
--- WebApplication2/Models/Degree.cs
+++ WebApplication2/Models/Degree.cs
@@ -9,8 +9,11 @@ namespace WebApplication2.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int DegreeId { get; set; }
+        [StringLength(10)]
         public string DegreeAbbr { get; set; }
+        [StringLength(20)]
         public string DegreeName { get; set; }
+        [Range(0, 8)]
         public int NumberOfTerms { get; set; }
 
 
